116f70cacd188eb5a90322ffc56778587e5150f5
mimof <mimof@windows10.microdone.cn>
2018-09-04 15:57:38 +0800

    用户管理已经完成 后台用户管理只有查询权限

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.util.List;
+
+import pojo.User;
+
+
+public interface UserService {
+	void add(User c);
+    void delete(int id);
+    void update(User c);
+    User get(int id);
+	List list();
+}
---
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mapper.UserMapper;
+import pojo.User;
+import pojo.UserExample;
+import service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	@Autowired
+	UserMapper userMapper;
+	
+	@Override
+    public void add(User u) {
+        userMapper.insert(u);
+    }
+ 
+    @Override
+    public void delete(int id) {
+        userMapper.deleteByPrimaryKey(id);
+    }
+ 
+    @Override
+    public void update(User u) {
+        userMapper.updateByPrimaryKeySelective(u);
+    }
+ 
+    @Override
+    public User get(int id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+	@Override
+	public List<User> list() {
+		UserExample example = new UserExample();
+		example.setOrderByClause("id desc");
+		return userMapper.selectByExample(example);
+	}
+
+}
